b487095e24f287c7cbee41c8ff4082a9095e2996
Disorder <disorrder@gmail.com>
2016-11-14 01:26:51 +0300

    add Keyboard
    problem: keydown and keyup write letter same. (trigger twice). SendMessage not working

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const au = require('autoit');
 const robotjs = require('robotjs');
 const screen = require('./src/screen');
 const Window = require('./src/window');
+const Keyboard = require('./src/keyboard');
 
 // --- tests init ---
 var tmpDir = './.tmp';
@@ -14,13 +15,27 @@ try {
 // --- tests here ---
 console.log('screen size:', screen.width, screen.height);
 
-var pid = Window.open('notepad.exe');
-console.log('pid', pid);
-au.Sleep(500);
-var wnd = Window.findByPid(pid);
+var wnd = Window.find('Безымянный');
+
+// var pid = Window.open('notepad.exe');
+// console.log('pid', pid);
+// au.Sleep(500);
+// var wnd = Window.findByPid(pid);
+
 console.log(wnd.title);
-wnd.setPos(0, 0, 300, 300);
+// wnd.setPos(0, 0, 300, 300);
 // wnd.hide().show().blur().focus()//.minimize().kill();
 // robotjs.typeStringDelayed('Bitch, why you cant write russian?!\n', 600)
 // robotjs.typeStringDelayed('АБЫРВАЛГ', 600)
-wnd.kill();
+
+// var kb = new Keyboard(wnd.hwnd);
+var kb = new Keyboard(wnd.getChildren()[0].hwnd);
+kb.keydown('a');
+kb.keyup('a');
+kb.pressKey('b');
+kb._char('c');
+
+console.log('classes:', wnd.getClassList());
+console.log(wnd.getChildren());
+
+// wnd.kill();
---
+++ src/keyboard.js
@@ -0,0 +1,122 @@
+const _ = require('lodash');
+const code = require('./apiConst');
+const au = require('autoit');
+const robotjs = require('robotjs');
+
+var keys = {
+    'back':     code.VK_BACKSPACE,
+    'enter':    code.VK_RETURN,
+    'ctrl':     code.VK_CONTROL,
+    'caps':     code.VK_CAPITAL,
+    'capslock': code.VK_CAPITAL,
+    'esc':      code.VK_ESCAPE,
+    ' ':        code.VK_SPACE,
+    'pageup':   code.VK_PRIOR,
+    'pgup':     code.VK_PRIOR,
+    'pagedown': code.VK_NEXT,
+    'pgdn':     code.VK_NEXT,
+    'printscreen':  code.VK_SNAPSHOT,
+    'prtscr':   code.VK_SNAPSHOT,
+    'ins':      code.VK_INSERT,
+    'del':      code.VK_DELETE,
+    'win':      code.VK_LWIN,
+    '*':        code.VK_MULTIPLY,
+    '+':        code.VK_ADD,
+    '|':        code.VK_SEPARATOR,
+    '-':        code.VK_SUBTRACT,
+    '.':        code.VK_DECIMAL,
+    '/':        code.VK_DIVIDE,
+    'num':      code.VK_NUMLOCK,
+    'lctrl':    code.VK_LCONTROL,
+    'rctrl':    code.VK_RCONTROL,
+    'lalt':     code.VK_LMENU,
+    'ralt':     code.VK_RMENU,
+}
+
+for (let key in code) {
+    if (key.indexOf('VK_') < 0) continue;
+    let keyName = key.substr(3).toLowerCase();
+    keys[keyName] = code[key];
+}
+
+const EN = "`qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+const RU = "ёйцукенгшщзхъфывапролджэячсмитьбю.";
+
+class Keyboard {
+    constructor(hwnd) {
+        this.hwnd = hwnd;
+        this.delay = 300;
+
+        // this._ctrl = false;
+        // this._alt = false;
+        // this._shift = false;
+        // this._win = false;
+    }
+
+    get ctrl() { return this._ctrl; }
+    set ctrl(val) { val ? this.keydown('ctrl') : this.keyup('ctrl'); }
+
+    get alt() { return this._alt; }
+    set ctrl(val) { val ? this.keydown('alt') : this.keyup('alt'); }
+
+    get shift() { return this._shift; }
+    set ctrl(val) { val ? this.keydown('shift') : this.keyup('shift'); }
+
+    get win() { return this._win; }
+    set ctrl(val) { val ? this.keydown('win') : this.keyup('win'); }
+
+    keydown(keyCode) {
+        if (_.isString(keyCode)) keyCode = keys[keyCode];
+        ['ctrl', 'alt', 'shift', 'win'].forEach((v) => {
+            if (keyCode === keys[v]) this[`_${v}`] = true;
+        });
+        au.PostMessage(this.hwnd, au.WM_KEYDOWN, keyCode, 0);
+        // au.SendMessage(this.hwnd, au.WM_KEYDOWN, keyCode, MapVirtualKey(keyCode));
+        console.log('keydown', keyCode.toString(16), MapVirtualKey(keyCode));
+        if (this.delay) au.Sleep(this.delay);
+    }
+
+    keyup(keyCode) {
+        if (_.isString(keyCode)) keyCode = keys[keyCode];
+        ['ctrl', 'alt', 'shift', 'win'].forEach((v) => {
+            if (keyCode === keys[v]) this[`_${v}`] = false;
+        });
+        au.PostMessage(this.hwnd, au.WM_KEYUP, keyCode, 0);
+        // au.SendMessage(this.hwnd, au.WM_KEYUP, keyCode, MapVirtualKey(keyCode));
+        console.log('keyup', keyCode.toString(16), MapVirtualKey(keyCode));
+        if (this.delay) au.Sleep(this.delay);
+    }
+
+    _char(keyCode) {
+        if (_.isString(keyCode)) keyCode = keys[keyCode];
+        au.PostMessage(this.hwnd, au.WM_CHAR, keyCode, 0);
+        // au.SendMessage(this.hwnd, au.WM_KEYUP, keyCode, MapVirtualKey(keyCode));
+        console.log('char', keyCode.toString(16), MapVirtualKey(keyCode));
+        if (this.delay) au.Sleep(this.delay);
+    }
+
+    pressKey(keyCode, modifier, holdTime) {
+        this.keydown(keyCode);
+        if (holdTime) au.Sleep(holdTime);
+        this.keyup(keyCode);
+    }
+
+    typeString(string, cpm) {
+
+    }
+
+}
+
+function MapVirtualKey(code, mapType = 0) {
+    var ref = require('ref');
+    var ffi = require('ffi');
+
+    var user32 = ffi.Library('user32.dll', {
+        MapVirtualKeyW: ['uint32', ['uint32', 'uint32']]
+    });
+
+    return user32.MapVirtualKeyW(code, mapType);
+}
+
+
+module.exports = Keyboard;
